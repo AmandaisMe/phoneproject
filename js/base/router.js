aaf6bb80997afd39cc4324800f64c6f23d33f7f2
13160801434@163.com <13160801434@163.com>
2017-03-04 18:06:09 +0800

    allproject

---
+++ js/base/router.js
@@ -0,0 +1,182 @@
+var app = angular.module('traveApp', ['routers', 'Ctrl', 'directive','service']);
+
+var router = angular.module('routers', ['ui.router']);
+router.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('index', {
+            url: '/index',
+            templateUrl: 'template/index.html',
+            controller: 'indexCtrl'
+        })
+        .state('index.home', {
+            url: '/home',
+            templateUrl: 'template/home.html',
+            controller: 'homeCtrl'
+
+        }).state('index.message', {
+            url: '/message',
+            templateUrl: 'template/message.html'
+        }).state('index.comment', {
+            url: '/comment',
+            templateUrl: 'template/comment.html'
+        }).state('index.page', {
+            url: '/page',
+            templateUrl: 'template/page.html'
+        }).state('detail', {
+            url: '/detail/:id',
+            templateUrl: 'template/detail.html',
+            controller: 'detailCtrl'
+        })
+
+    $urlRouterProvider.otherwise('/index/home')
+}])
+
+//控制器
+var controller = angular.module('Ctrl', []);
+controller.controller('indexCtrl', ['$scope', function($scope) {
+    $scope.items = [{
+        title: '首页',
+        url: '/home',
+        src: 'images/首页-发现.png'
+    }, {
+        title: '消息',
+        url: '/message',
+        src: 'images/message.png'
+    }, {
+        title: '言职',
+        url: '/comment',
+        src: 'images/文件1.png'
+
+    }, {
+        title: '我',
+        url: '/page',
+        src: 'images/首页-我的.png'
+
+    }]
+}]).controller('homeCtrl', ['$scope', '$http', 'xcarousel',function($scope, $http,xcarousel) {
+    $scope.searchname = ''
+    $scope.search = false;
+    // $scope.carous=;
+    // console.log($scope.carous)
+    $scope.searchtext = function() {
+        $scope.search = true;
+    }
+    $scope.cancle = function() {
+        $scope.search = false;
+    }
+
+    $scope.arrs = [];
+    // $scope.show=function(){
+    //      xcarousel.carousel()
+    // }
+    $scope.loadmore = function() {
+    
+        $http.get('js/list.json', {
+            params: {
+                id: $scope.id,
+                title: $scope.title,
+                company: $scope.conpany,
+                money: $scope.money,
+                respon: $scope.respon,
+                weal: $scope.weal,
+                size: $scope.size
+                    // detail:$scope.detail
+
+
+            }
+        }).success(function(data) {
+             xcarousel.carousel()
+
+            $scope.arrs = $scope.arrs.concat(data);
+            console.log($scope.arrs)
+        })
+    }
+    $scope.loadmore();
+
+}]).controller('detailCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
+
+    console.log($state.params.id)
+    $http.get('js/detail' + $state.params.id + '.json', {
+        params: {
+            id: $state.params.id,
+            // detail:$scope.params.detail
+
+        }
+    }).success(function(data) {
+
+        $scope.arr = data[0];
+        console.log($scope.arr)
+
+
+    })
+}])
+
+//directive
+var direc = angular.module('directive', []);
+direc.directive('xfooter', function() {
+    return {
+        templateUrl: 'directive/xfooter.html'
+    }
+}).directive('xsearch', function() {
+    return {
+        templateUrl: 'directive/xsearch.html'
+    }
+}).directive('xcarousel', function() {
+    return {
+        // templateUrl: 'directive/xcarousel.html',
+        link: function() {
+            var swiper = new Swiper('.swiper-container', {
+                pagination: '.swiper-pagination',
+                effect: 'cube',
+                grabCursor: true,
+                cube: {
+                    shadow: true,
+                    slideShadows: true,
+                    shadowOffset: 20,
+                    shadowScale: 0.94
+                }
+            });
+        }
+    }
+}).directive('xitem', function() {
+    return {
+        templateUrl: 'directive/xitem.html'
+    }
+}).directive('ngScroll', function() {
+
+    return {
+        link: function(scope, ele, attr) {
+            console.log(ele);
+            ele.bind('scroll', function(e) {
+                console.log(e.target.scrollTop)
+                    // console.log(e.target.scrollHeight)
+                    // console.log(e.target.offsetTop)
+                if ((e.target.scrollTop + e.target.offsetHeight) >= e.target.scrollHeight) {
+                    console.log(1)
+                    scope.$apply(attr.ngScroll);
+                    console.log(scope.$apply(attr.ngScroll))
+                }
+
+
+            })
+        }
+    }
+})
+
+var app=angular.module('service',[]);
+app.service('xcarousel',function(){
+   return{
+     carousel: function() {
+            var swiper = new Swiper('.swiper-container', {
+                pagination: '.swiper-pagination',
+                effect: 'cube',
+                grabCursor: true,
+                cube: {
+                    shadow: true,
+                    slideShadows: true,
+                    shadowOffset: 20,
+                    shadowScale: 0.94
+                }
+            })
+        }
+   }
+})
